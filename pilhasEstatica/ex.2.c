8bc9c26ee70dde33a0f9fe9e27a18a0ba3805fc1
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-09-12 13:45:06 -0300

    feat: emmpilhando elementos

--- pilhasEstatica/ex.2.c
+++ pilhasEstatica/ex.2.c
@@ -4,11 +4,21 @@
 
 #define MAXTAM 10
 
-//molde para os objetos
+/**
+ * Exercício 2: A conversão de um número decimal para sua representação
+ * binária é feita através de divisões sucessivas por 2. 
+ */
+
+//molde para os objetos - dados abstratos
 typedef struct{
-    int vetor[MAXTAM];
+  int decimal;
+} Objeto;
+
+typedef struct{
+    Objeto vetor[MAXTAM];
     int topo;
 } PilhaEstatica;
+
 
 //inicializar pilha
 void inicializaPilha(PilhaEstatica *p) { p->topo = 0; }
@@ -19,13 +29,34 @@ bool estaVazia(PilhaEstatica *p) { return (p->topo == 0); }
 //verifica se esta cheia
 bool estaCheia(PilhaEstatica *p) { return (p->topo == MAXTAM); }
 
+void push(PilhaEstatica *p, int decimal){
+    if(!estaCheia(p)){
+      p->vetor[p->topo].decimal = decimal;  // topo é a ultia possicao livre
+      p->topo++;//atualiza ponteiro
+    }
+}
+
+void imprimirPilha(PilhaEstatica *p) { 
+    for (int i = 0; i < p->topo; i++){
+      printf("%d ", p->vetor[i].decimal);
+    }
+    printf("\n");
+}
+
 int main() {
     PilhaEstatica pObjeto; //criando/instanciando uma variável do tipo PilhaEstatica
-    inicializaPilha(&pObjeto);
+    inicializaPilha(&pObjeto); // passando endereco de meoria da variavel
 
-    if(estaVazia){
-      printf("Pilha Vazia! ");
+    if(estaVazia(&pObjeto)){
+      printf("Pilha Vazia!\n");
     }
 
-    return 0; 
+    //empilhar eleentos na pilha
+    int decimal[] = {13, 6, 3, 1};
+    for (int i = 0; i < 4; i++){
+      push(&pObjeto, decimal[i]);
+      imprimirPilha(&pObjeto);
+    } 
+
+    return 0;
 }
