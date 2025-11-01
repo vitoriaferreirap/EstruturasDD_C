832b4d2cf69a0be3f18a9abe4aa18c6ebc17a514
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-11-01 14:43:27 -0300

    feat: excluindo elemento da lista, atualizando a quantidade e limpando a memoria

--- ListasOrdenadas/LinkedList.c
+++ ListasOrdenadas/LinkedList.c
@@ -64,6 +64,44 @@ void imprimir(Lista *p){
     printf("\n");
 }
 
+void excluir(Lista *p, int valorBuscado){
+    //cria variavel local para ser o nosso ponteiro
+    Node atual = p->head;
+    Node anterior = NULL;
+
+    //enquanto atual for == null, chegamos ao fim da lista
+    //enquanto onde o atual esta o dados for diferente do que busco
+    //while para quando o valor é encontrado ou quando chega no fim da lista.
+    while(atual != NULL && atual->dado != valorBuscado){
+        //atual e anterior caminham juntos, MAS preciso que o anterior esteka uma casa antes do atual
+        //logo, o anterior sempre pega a posição do atual antes do atual andar.
+        //anterior aponta para onde o atual aponta
+        anterior = atual;
+        //atual, atualizado aponta para onde o ponteiro da proxima cx aponta
+        atual = atual->proximo;
+    }
+
+    //se atual == Null nao achamos o valor
+    if(atual == NULL){
+      printf("Valor não encontrado!");
+      return;
+    }
+
+    //se atual != Null achamos o valor, devos usar o free para remover o no encontrado com o valor que busco
+    if(anterior == NULL){
+        //Nó que sera removido sera o head(numero que buscamos esta na primeira posicao)
+        p->head = atual->proximo;
+        free(atual);
+        p->qtd--;
+    }else{
+        //valor e no qualquer no meio da lista
+        anterior->proximo = atual->proximo; //atualizamos anterior->proximo para “pular” o nó removido.
+        free(atual);
+        p->qtd--;
+    }
+
+}
+
 int main(){
 
     //variavel tipo Lista
@@ -75,11 +113,19 @@ int main(){
     //ver tamanho da lista
     int tam = tamanho(&minhaLista);
     printf("Tamanho da lista: %d \n", tam);
-    
+
     imprimir(&minhaLista);
 
     inserir(&minhaLista, 1);
     inserir(&minhaLista, 2);
+    inserir(&minhaLista, 3);
+
+    tam = tamanho(&minhaLista);
+    printf("Tamanho da lista: %d \n", tam);
+
+    imprimir(&minhaLista);
+
+    excluir(&minhaLista, 2);
 
     tam = tamanho(&minhaLista);
     printf("Tamanho da lista: %d \n", tam);
